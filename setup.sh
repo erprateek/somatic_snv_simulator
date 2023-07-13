906135d3cc351e23f16e68c59d597cebc8aa84dc
Prateek Tandon <erprateek.vit@gmail.com>
2023-07-13 15:42:32 -0700

    add yes to conda create command

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 env_name=somatic_snv_sim
-conda create -n $env_name python=3.10
+conda create -n $env_name python=3.10 --yes
 conda activate $env_name
 pip install -r requirements.txt
