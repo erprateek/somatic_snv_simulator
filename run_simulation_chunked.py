d57a86de0f1a8b29b786b81cf0275444ccf76c45
Prateek Tandon <erprateek.vit@gmail.com>
2023-07-12 17:11:48 -0700

    working chunker

--- run_simulation_chunked.py
+++ run_simulation_chunked.py
@@ -5,31 +5,33 @@ import psutil
 
 def get_available_memory():
     mem = psutil.virtual_memory()
-    return mem.available
+    return int(mem.available*0.6)
 
 def simulate_somatic_mutations_chunk(chunk_size, depth, error_rate, allele_fraction, alt_threshold, fv):
     # Simulate total read depth from a Poisson distribution
-    total_depth = np.random.poisson(depth, size=chunk_size)
+    total_depth = np.random.poisson(depth, size=chunk_size).astype('uint8')
 
     # Simulate sequencing noise from a Binomial distribution
-    noise_reads = np.random.binomial(total_depth, error_rate)
+    noise_reads = np.random.binomial(total_depth, error_rate).astype('uint8')
 
     # Simulate somatic mutation (if any)
-    has_mutation = np.random.choice([True, False], size=chunk_size, p=[allele_fraction, 1-allele_fraction])
-    mutation_reads = np.random.binomial(total_depth, allele_fraction) * (np.random.rand(chunk_size) < fv) * has_mutation
+    has_mutation = np.random.choice([True, False], size=chunk_size, p=[fv, 1-fv])
+    mutation_reads = np.random.binomial(total_depth, allele_fraction).astype('uint8') * has_mutation
 
     # Calculate ALT reads (sequencing noise + somatic mutation)
     alt_reads = noise_reads + mutation_reads
 
     # Determine if somatic variant is detected
-    is_positive = alt_reads >= alt_threshold
+    #is_positive = alt_reads >= alt_threshold
 
-    return alt_reads, is_positive
+    return alt_reads, has_mutation
 
 def calculate_metrics(labels, predictions):
-    true_positive = np.sum(labels & predictions)
+    labels = labels.astype('uint64')
+    predictions = predictions.astype('uint64')
+    true_positive  =  np.sum(labels & predictions)
     false_positive = np.sum(~labels & predictions)
-    true_negative = np.sum(~labels & ~predictions)
+    true_negative  = np.sum(~labels & ~predictions)
     false_negative = np.sum(labels & ~predictions)
 
     return true_positive, false_positive, true_negative, false_negative
@@ -43,13 +45,20 @@ def plot_metrics(allele_fractions, ppas, ppvs):
     plt.show()
 
 # Parse command-line arguments
-parser = argparse.ArgumentParser(description='Monte-Carlo simulation of somatic mutations in NGS data')
-parser.add_argument('--num_simulations', type=int, default=1000, help='Number of simulations')
-parser.add_argument('--depth', type=int, default=50, help='Total read depth')
-parser.add_argument('--error_rate', type=float, default=0.05, help='Sequencing error rate')
-parser.add_argument('--alt_threshold', type=int, default=3, help='ALT threshold')
-parser.add_argument('--allele_fractions', nargs='+', type=float, default=[0.1], help='Allele fractions to test')
-parser.add_argument('--fv', type=float, default=0.5, help='Fraction of k pileups with a somatic variant')
+parser = argparse.ArgumentParser(
+    description='Monte-Carlo simulation of somatic mutations in NGS data')
+parser.add_argument('--num_simulations', type=int, default=1000, 
+                    help='Number of simulations')
+parser.add_argument('--depth', type=int, default=50, 
+                    help='Total read depth')
+parser.add_argument('--error_rate', type=float, default=0.05, 
+                    help='Sequencing error rate')
+parser.add_argument('--alt_threshold', type=int, default=3, 
+                    help='ALT threshold')
+parser.add_argument('--allele_fractions', nargs='+', type=float, default=[0.1],
+                    help='Allele fractions to test')
+parser.add_argument('--fv', type=float, default=0.5, 
+                    help='Fraction of k pileups with a somatic variant')
 
 args = parser.parse_args()
 
@@ -61,7 +70,7 @@ available_memory = get_available_memory()
 
 # Calculate the ideal chunk size based on available memory
 total_simulations = args.num_simulations
-ideal_chunk_size = min(total_simulations, available_memory // 8)  # Assuming 8 bytes per sample
+ideal_chunk_size = min(total_simulations, available_memory // 24)  # Assuming 8 bytes per sample
 
 # Calculate the number of chunks and remaining simulations
 num_chunks = total_simulations // ideal_chunk_size
@@ -110,37 +119,26 @@ for allele_fraction in args.allele_fractions:
 
     # Handle remaining simulations (if any)
     if remaining_simulations > 0:
-        pileups, labels = simulate_somatic_mutations_chunk(remaining_simulations, depth, error_rate, allele_fraction, alt_threshold)
-        predictions = pileups >= alt_threshold
+        pileups, labels = simulate_somatic_mutations_chunk(remaining_simulations, args.depth, args.error_rate, allele_fraction, args.alt_threshold, args.fv)
+        predictions     = pileups >= args.alt_threshold
         true_positive, false_positive, true_negative, false_negative = calculate_metrics(labels, predictions)
         
-        true_positive_total += true_positive
-        false_positive_total += false_positive
-        true_negative_total += true_negative
-        false_negative_total += false_negative
+        true_positive_sum  += true_positive
+        false_positive_sum += false_positive
+        true_negative_sum  += true_negative
+        false_negative_sum += false_negative
+
+    total_samples = total_simulations
+    confusion_matrix = np.array([[true_negative_sum, false_positive_sum], [false_negative_sum, true_positive_sum]])
+    ppa = np.array(true_positive_sum) / (np.array(true_positive_sum) + np.array(false_negative_sum))
+    ppv = np.array(true_positive_sum) / (np.array(true_positive_sum) + np.array(false_positive_sum))
+    specificity = np.array(true_negative_sum) / (np.array(true_negative_sum) + np.array(false_positive_sum))
 
 
-    # Accumulate metrics for the current allele fraction
-    true_positive_total.append(true_positive_sum)
-    false_positive_total.append(false_positive_sum)
-    true_negative_total.append(true_negative_sum)
-    false_negative_total.append(false_negative_sum)
+    print("Confusion Matrix:")
+    print(confusion_matrix)
+    print("PPA:", round(ppa*100,2))
+    print("PPV:", round(ppv*100,2))
+    print("Specificity:", round(specificity*100,2))
 
-# Calculate overall metrics
-#ppas = np.array(true_positive_total) / (np.array(true_positive_total) + np.array(false_negative_total))
-#ppvs = np.array(true_positive_total) / (np.array(true_positive_total) + np.array(false_positive_total))
-
-total_samples = total_simulations
-confusion_matrix = np.array([[true_negative_total, false_positive_total], [false_negative_total, true_positive_total]])
-ppas = np.array(true_positive_total) / (np.array(true_positive_total) + np.array(false_negative_total))
-ppvs = np.array(true_positive_total) / (np.array(true_positive_total) + np.array(false_positive_total))
-specificities = np.array(true_negative_total) / (np.array(true_negative_total) + np.array(false_positive_total))
-
-print("Confusion Matrix:")
-print(confusion_matrix)
-print("PPA:", ppas)
-print("PPV:", ppvs)
-print("Specificity:", specificities)
-
-# Plot metrics
-plot_metrics(args.allele_fractions, ppas, ppvs)
+    # Plot metrics
